cb43a4763e77a00cc935c53f561ac0401e3d5d6d
sikderj3596 <sikderj3596@ATC15620>
2016-10-18 11:14:15 -0400

    initial commit with command parser started

---
+++ textAdventureTest/textAdventureTest/CommandParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textAdventureTest
+{
+    class CommandParser
+    {
+        public CommandParser(string command)
+        {
+            string lowerCommand = command.ToLower();
+            string[] commandElements = lowerCommand.Split(' ');
+            switch (commandElements[0])
+            {
+                case "go":
+                    {
+                        switch (commandElements[1])
+                        {
+                            case "north":
+                                {
+                                    //code to move north
+                                    break;
+                                }
+                            case "south":
+                                {
+                                    //code to move south
+                                    break;
+                                }
+                            case "west":
+                                {
+                                    //code to move west
+                                    break;
+                                }
+                            case "east":
+                                {
+                                    //code to move east
+                                    break;
+                                }
+                            default:
+                                {
+                                    
+                                    break;
+                                }
+
+                        }
+                    break;
+                    }
+            }
+
+
+
+        }
+    }
+}
